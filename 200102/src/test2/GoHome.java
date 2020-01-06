15fc2fd7dddb1433e97701ecb1571f25435b4335
7 <7@7-PC>
2020-01-06 10:45:34 +0900

    비행기예약

---
+++ 200102/src/test2/GoHome.java
@@ -0,0 +1,18 @@
+package test2;
+
+public class GoHome {
+		
+		public void start () {
+			GoHomeProgram program = new GoHomeProgram();
+			program.startProgram();
+		}
+
+		public static void main(String[] args) {
+			
+			GoHome gohome = new GoHome();
+			gohome.start();
+
+		}
+	
+	
+}
